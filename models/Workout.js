fc6c0b74b44e540804dcccfd8e66af6941b0eced
Amanda Hamilton <77996281+polysnacktyl@users.noreply.github.com>
2021-04-22 17:32:35 -0400

    added workout model properties

--- models/Workout.js
+++ models/Workout.js
@@ -5,7 +5,38 @@ const WorkoutSchema = mongoose.Schema({
     author: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
-    }
+    },
+    day: {
+        type: String,
+        default: () => new Date()
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "specify a workout type"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "enter workout name"
+            },
+            duration: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            duration: {
+                sets: Number
+            }
+        },
+
+    ]
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
