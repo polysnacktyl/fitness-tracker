b9247a53a5a9c5afea6385dfb9c8cd3d422659b1
Amanda Hamilton <77996281+polysnacktyl@users.noreply.github.com>
2021-04-23 17:04:47 -0400

    correcting db name in several instances

--- routes/api.js
+++ routes/api.js
@@ -1,35 +1,72 @@
 const router = require("express").Router();
 const Workout = require("../models/Workout.js");
 
-router.post("/api/workout", ({ body }, res) => {
+router.post("/api/workouts", ({ body }, res) => {
   Workout.create(body)
-    .then(dbWorkout => {
-      res.json(dbWorkout);
+    .then(workout => {
+      res.json(workout);
     })
     .catch(err => {
       res.status(400).json(err);
     });
 });
 
-router.post("/api/workout/bulk", ({ body }, res) => {
-  Workout.insertMany(body)
-    .then(dbWorkout => {
-      res.json(dbWorkout);
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
+  )
+    .then(workout => {
+      res.json(workout);
     })
     .catch(err => {
-      res.status(400).json(err);
+      res.json(err);
     });
 });
 
 router.get("/api/workouts", (req, res) => {
-  Workout.find({})
-    .sort({ date: -1 })
-    .then(dbWorkout => {
-      res.json(dbWorkout);
+  Workout.find()
+    .then(workout => {
+      res.json(workout);
     })
     .catch(err => {
-      res.status(400).json(err);
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .then(workout => {
+      res.json(workout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.delete("/api/workouts", ({ body }, res) => {
+  Workout.findByIdAndDelete(body.id)
+    .then(() => {
+      res.json(true);
+    })
+    .catch(err => {
+      res.json(err);
     });
 });
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
