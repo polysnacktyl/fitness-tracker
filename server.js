2961c1cf1819b174ec27a50d26b652ab60d1a38f
Amanda Hamilton <77996281+polysnacktyl@users.noreply.github.com>
2021-04-23 16:37:28 -0400

    tidying and adding unifiedtopology jam on line 17 to prevent mongoose warning on server start

--- server.js
+++ server.js
@@ -1,34 +1,41 @@
 const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
+const db = require("./models");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
-
-const db = require("./models");
-
-const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useUnifiedTopology: true,
   useNewUrlParser: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
-db.Workout.create({ name: 'workout'})
-.then(dbWorkout => {
-  console.log(dbWorkout);
-})
-.catch(({message}) => {
-console.log(message);
+db.Workout.create({ name: "workout" })
+  .then(workout => {
+    console.log(workout);
+  })
+  .catch(({message}) => {
+    console.log(message);
+  });
 
-});
 
-app.use(require("./routes/api.js"));
-app.use(require("./routes/view"))
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
+
+
+
+
+
