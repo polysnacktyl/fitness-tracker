5f20e02a3e6360a014fab6e5f4506efe092494c8
Amanda Hamilton <the.a.hamilton@gmail.com>
2021-04-24 12:01:17 -0400

    oh wow my /workouts get route was wrong the whole time

--- routes/api.js
+++ routes/api.js
@@ -2,57 +2,54 @@ const router = require("express").Router();
 const Workout = require("../models/Workout.js");
 
 router.post("/api/workouts", ({ body }, res) => {
-    Workout.create(body)
-        .then(workout => {
-            res.json(workout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
+  Workout.create(body)
+    .then(workout => {
+      res.json(workout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
 });
 
 router.put("/api/workouts/:id", ({ body, params }, res) => {
-    Workout.findByIdAndUpdate(
-        params.id,
-        { $push: { exercises: body } },
-        { new: true, runValidators: true }
-    )
-        .then(workout => {
-            res.json(workout);
-        })
-        .catch(err => {
-            res.json(err);
-        });
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
+  )
+    .then(workout => {
+      res.json(workout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 router.get("/api/workouts/range", ({ body }, res) => {
-    Workout.aggregate([
-        {
-            $addFields: {
-                totalDuration: { $sum: "$exercises.duration" },
-                totalSets: { $sum: "$exercises.sets" },
-                totalWeight: { $sum: "$exercises.weight" },
-                totalReps: { $sum: "$exercises.reps" },
-                totalDistance: { $sum: "$exercises.distance" }
-            }
-        }
-    ])
-        .then(workout => {
-            res.json(workout);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+        totalWeight: { $sum: "$exercises.weight" },
+        totalSets: { $sum: "$exercises.sets" },
+        totalReps: { $sum: "$exercises.reps" },
+        totalDistance: { $sum: "$exercises.distance" }
+      }
+    }
+  ])
+    .then(workout => {
+      res.json(workout);
+    });
+  });
 
-router.get("/api/workouts/range", (req, res) => {
-    Workout.find({})
-        .then(workout => {
-            res.json(workout);
-        })
-        .catch(err => {
-            res.json(err);
-        });
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .then(workout => {
+      res.json(workout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
