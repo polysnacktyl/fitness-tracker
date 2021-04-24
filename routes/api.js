33e8ecb46084181b98fc793d5a2a8060ee337500
Amanda Hamilton <77996281+polysnacktyl@users.noreply.github.com>
2021-04-23 22:37:36 -0400

    added route for api/workouts/range so the left graph would left graph

--- routes/api.js
+++ routes/api.js
@@ -25,15 +25,22 @@ router.put("/api/workouts/:id", ({ body, params }, res) => {
     });
 });
 
-router.get("/api/workouts", (req, res) => {
-  Workout.find()
-    .then(workout => {
-      res.json(workout);
-    })
-    .catch(err => {
-      res.json(err);
+router.get("/api/workouts/range", ({ body }, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+        totalWeight: { $sum: "$exercises.weight" },
+        totalSets: { $sum: "$exercises.sets" },
+        totalReps: { $sum: "$exercises.reps" },
+        totalDistance: { $sum: "$exercises.distance" }
+      }
+    }
+  ])
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     });
-});
+  });
 
 router.get("/api/workouts/range", (req, res) => {
   Workout.find({})
@@ -45,28 +52,14 @@ router.get("/api/workouts/range", (req, res) => {
     });
 });
 
-router.delete("/api/workouts", ({ body }, res) => {
-  Workout.findByIdAndDelete(body.id)
-    .then(() => {
-      res.json(true);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
+// router.delete("/api/workouts", ({ body }, res) => {
+//   Workout.findByIdAndDelete(body.id)
+//     .then(() => {
+//       res.json(true);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
 
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
